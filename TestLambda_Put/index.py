22373dca12d48c435cdf8c951c9fa3d8bafd5ed3
telledonnn <sargedines@gmail.com>
2025-08-17 18:52:48 +0800

    Create PUT API endpoint
    Put API endpoint calls a mock Put endpoint.

---
+++ TestLambda_Put/index.py
@@ -0,0 +1,14 @@
+import requests
+
+#REST URI
+url = "https://jsonplaceholder.typicode.com/posts/1"
+
+payload = {
+    "id" : 1,
+    "title" : "foo",
+    "body"  : "b3r3",
+    "userId" : 1
+}
+response = requests.put(url, payload)
+
+print(response.json())
